bcc52eaab43aea4ddb9456ef89c5fd4deaa9618b
tdeheurles <tdeheurles@gmail.com>
2021-03-30 17:43:12 +0200

    github-action test (2)

--- components/docker/build_and_push.sh
+++ components/docker/build_and_push.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo hello
+build_number=${BUILD_NUMBER:?$(echo -e "\e[92mPlease provide environment variable BUILD_NUMBER\e[39m" && exit 1)}       # CI_COMMIT_SHORT_SHA
+build_pipeline=${BUILD_PIPELINE:?$(echo -e "\e[92mPlease provide environment variable BUILD_PIPELINE\e[39m" && exit 1)} # CI_PIPELINE_ID
+git_branch=${GIT_BRANCH:?$(echo -e "\e[92mPlease provide environment variable GIT_BRANCH\e[39m" && exit 1)}             # CI_COMMIT_BRANCH
+git_short_sha=${GIT_SHORT_SHA:?$(echo -e "\e[92mPlease provide environment variable GIT_SHORT_SHA\e[39m" && exit 1)}    # CI_COMMIT_SHORT_SHA
+docker_registry=${DOCKER_REGISTRY:?$(echo -e "\e[92mPlease provide environment variable SERVICE_NAME\e[39m" && exit 1)}
+semantic_versioning=${SEMANTIC_VERSIONING:?$(echo -e "\e[92mPlease provide environment variable SEMANTIC_VERSIONING\e[39m" && exit 1)}
+docker_username=${DOCKER_USERNAME:?$(echo -e "\e[92mPlease provide environment variable DOCKER_USERNAME\e[39m" && exit 1)}
+docker_password=${DOCKER_PASSWORD:?$(echo -e "\e[92mPlease provide environment variable DOCKER_PASSWORD\e[39m" && exit 1)}
 
-#build_number=${BUILD_NUMBER:?$(echo -e "\e[92mPlease provide environment variable BUILD_NUMBER\e[39m" && exit 1)}       # CI_COMMIT_SHORT_SHA
-#build_pipeline=${BUILD_PIPELINE:?$(echo -e "\e[92mPlease provide environment variable BUILD_PIPELINE\e[39m" && exit 1)} # CI_PIPELINE_ID
-#git_branch=${GIT_BRANCH:?$(echo -e "\e[92mPlease provide environment variable GIT_BRANCH\e[39m" && exit 1)}             # CI_COMMIT_BRANCH
-#git_short_sha=${GIT_SHORT_SHA:?$(echo -e "\e[92mPlease provide environment variable GIT_SHORT_SHA\e[39m" && exit 1)}    # CI_COMMIT_SHORT_SHA
-#docker_registry=${DOCKER_REGISTRY:?$(echo -e "\e[92mPlease provide environment variable SERVICE_NAME\e[39m" && exit 1)}
-#semantic_versioning=${SEMANTIC_VERSIONING:?$(echo -e "\e[92mPlease provide environment variable SEMANTIC_VERSIONING\e[39m" && exit 1)}
-#docker_username=${DOCKER_USERNAME:?$(echo -e "\e[92mPlease provide environment variable DOCKER_USERNAME\e[39m" && exit 1)}
-#docker_password=${DOCKER_PASSWORD:?$(echo -e "\e[92mPlease provide environment variable DOCKER_PASSWORD\e[39m" && exit 1)}
-#
-#this_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-#project_root="${this_directory}/../.."
-#(
+this_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+project_root="${this_directory}/../.."
+(
+  echo "hello"
 #  mkdir -p /kaniko/.docker
 #  echo "{\"auths\":{\"$docker_registry\":{\"username\":\"$docker_username\",\"password\":\"$docker_password\"}}}" >/kaniko/.docker/config.json
 #
@@ -48,4 +47,4 @@ echo hello
 #
 #  echo -e "\e[36m------------ /kaniko/executor ------------\e[39m"
 #  ${command}
-#)
+)
