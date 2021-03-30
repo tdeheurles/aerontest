43588e9adb0a9ff547d3496561e33b034fc2b4db
tdeheurles <tdeheurles@gmail.com>
2021-03-30 17:53:50 +0200

    github-action test (5)

--- components/docker/build_and_push.sh
+++ components/docker/build_and_push.sh
@@ -12,38 +12,37 @@ docker_password=${DOCKER_PASSWORD:?$(echo -e "\e[92mPlease provide environment v
 
 this_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 project_root="${this_directory}/../.."
-(
-  mkdir -p /kaniko/.docker
-  echo "{\"auths\":{\"$docker_registry\":{\"username\":\"$docker_username\",\"password\":\"$docker_password\"}}}" >/kaniko/.docker/config.json
 
-  cd "${project_root}" || exit 1
-  ./do --host --component="dependencies" --command="get_node"
-  ./do --host --component="dependencies" --command="get_jdk"
-  ./do --host --component="dependencies" --command="get_jre"
+mkdir -p /kaniko/.docker
+echo "{\"auths\":{\"$docker_registry\":{\"username\":\"$docker_username\",\"password\":\"$docker_password\"}}}" >/kaniko/.docker/config.json
 
-  echo -e "\e[36m------------ Set variables ------------\e[39m"
-  image_id="${semantic_versioning}.${build_number}"
-  destination_1=${docker_registry}:${image_id}
-  destination_2=${docker_registry}:${semantic_versioning}
-  destination_3=${docker_registry}:${build_pipeline}
-  destination_4=${docker_registry}:${git_branch}
-  destination_5=${docker_registry}:${git_short_sha}
-  command="/kaniko/executor"
-  command="${command} --cache=true"
-  command="${command} --cache-repo ${docker_registry}"
-  command="${command} --context ${project_root}"
-  command="${command} --dockerfile ${this_directory}/Dockerfile"
-  command="${command} --destination ${destination_1}"
-  command="${command} --destination ${destination_2}"
-  command="${command} --destination ${destination_3}"
-  if [ -n "${CI_COMMIT_BRANCH##*/*}" ]; then
-    command="${command} --destination ${destination_4}"
-  fi
-  command="${command} --destination ${destination_5}"
+cd "${project_root}" || exit 1
+./do --host --component="dependencies" --command="get_node"
+./do --host --component="dependencies" --command="get_jdk"
+./do --host --component="dependencies" --command="get_jre"
 
-  echo -e "\e[36m------------ Variables dump ------------\e[39m"
-  echo "command: ${command}"
+echo -e "\e[36m------------ Set variables ------------\e[39m"
+image_id="${semantic_versioning}.${build_number}"
+destination_1=${docker_registry}:${image_id}
+destination_2=${docker_registry}:${semantic_versioning}
+destination_3=${docker_registry}:${build_pipeline}
+destination_4=${docker_registry}:${git_branch}
+destination_5=${docker_registry}:${git_short_sha}
+command="/kaniko/executor"
+command="${command} --cache=true"
+command="${command} --cache-repo ${docker_registry}"
+command="${command} --context ${project_root}"
+command="${command} --dockerfile ${this_directory}/Dockerfile"
+command="${command} --destination ${destination_1}"
+command="${command} --destination ${destination_2}"
+command="${command} --destination ${destination_3}"
+if [ -n "${CI_COMMIT_BRANCH##*/*}" ]; then
+  command="${command} --destination ${destination_4}"
+fi
+command="${command} --destination ${destination_5}"
 
-  echo -e "\e[36m------------ /kaniko/executor ------------\e[39m"
-  ${command}
-)
+echo -e "\e[36m------------ Variables dump ------------\e[39m"
+echo "command: ${command}"
+
+echo -e "\e[36m------------ /kaniko/executor ------------\e[39m"
+${command}
